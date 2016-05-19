88777767d5f8e180ab1f6c3d5754c37953c9e52b
KityPei <Kity_Pei@163.com>
2016-05-19 16:26:09 +0800

    创建新工程

---
+++ ShichaZ/ShichaZ/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ShichaZ
+//
+//  Created by Kity on 16/5/19.
+//  Copyright © 2016年 Kity_Pei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ShichaZ/ShichaZ/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ShichaZ
+//
+//  Created by Kity on 16/5/19.
+//  Copyright © 2016年 Kity_Pei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
